3994c5aba830f87be2ac11c46d7ef08ad4e1866c
Nathan Wise <nathanwise@gmail.com>
2012-08-11 13:05:43 -0400

    Creating class to handle building of button.

---
+++ app/d6_label_builder.rb
@@ -0,0 +1,14 @@
+class D6LabelBuilder
+
+  def self.build
+    @label = UILabel.new
+    @label.font = UIFont.systemFontOfSize(30)
+    @label.text = 'd6'
+    @label.textAlignment = UITextAlignmentCenter
+    @label.textColor = UIColor.whiteColor
+    @label.backgroundColor = UIColor.clearColor
+    @label.frame = [[20 + 60, 30], [80, 40]]
+    return @label
+  end
+
+end
--- app/dice_controller.rb
+++ app/dice_controller.rb
@@ -1,16 +1,20 @@
 class DiceController < UIViewController
   def viewDidLoad
     margin = 20
-    @d6 = D6.new
+    puts "initializing"
+    @d6 = D6LabelBuilder.build
+    puts "done init"
     #@d6 = UILabel.new
     #@d6.font = UIFont.systemFontOfSize(30)
     #@d6.text = '2d6'
     #@d6.textAlignment = UITextAlignmentCenter
     #@d6.textColor = UIColor.whiteColor
     #@d6.backgroundColor = UIColor.clearColor
-    #@d6.frame = [[margin + 60, 30], [50, 40]]
+    #@d6.frame = [[margin + 60, 30], [80, 40]]
+    puts @d6
+    puts UILabel.new
     view.addSubview(@d6)
-
+    puts 'added to subview'
     @d6_button = UIButton.buttonWithType(UIButtonTypeRoundedRect)
     @d6_button.setTitle('Roll', forState:UIControlStateNormal)
     @d6_button.addTarget(self, action:'d6_button_tapped', forControlEvents:UIControlEventTouchUpInside)
